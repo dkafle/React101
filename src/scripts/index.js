7d259308caebf303c505937c5d0fd5cb6dbeb57f
Deepak Kafle <dkafle@gmail.com>
2019-11-18 21:42:50 -0500

    Event handling with button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,23 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+// Data Types:
+// Number
+// String
+// Object
+// Boolean
+
+let count = 0;
+
+const incButton = document.querySelector('#increment');
+const decButton = document.querySelector('#decrement') ;
+const reult = document.querySelector('#result');
+
+incButton.onclick = function() {
+    count += 1;
+    result.innerText = count;
+};
+
+decButton.onclick = () => {
+    count -= 1;
+    result.innerText = count;
+};
