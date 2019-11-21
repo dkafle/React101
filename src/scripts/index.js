3f4245c1d0f12efb9d34d6b29dce229a89b2cab0
Deepak Kafle <dkafle@newitventure.com>
2019-11-20 22:22:23 -0500

    Function with callback

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,27 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+// Functions
+
+// Functions are Objects
+
+// Function are First Class Citizens
+
+const foo = cb => {
+  console.log('I am inside foo');
+  cb();
+}
+
+const bar = function () {
+  console.log('Surprise!');
+}
+
+const fullname = (f, l = 'Sharma') => `${f} ${l}`;
+
+const square = x => x * x;
+
+foo(bar);
+
+console.log(fullname('Ramesh', 'Rocks'));
+console.log(square(2))
