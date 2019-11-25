cc0a8248be556a3ecf78591f3f73b9bc2a3e63a0
Deepak Kafle <dkafle@newitventure.com>
2019-11-25 07:34:56 -0500

    Objects creation with literal and constructor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,35 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const Counter = function() {
+  let count = 0;
+
+  this.increment = function () {
+    count += 1;
+    return count;
+  };
+
+  this.decrement = function () {
+    count -= 1;
+    return count;
+  };
+
+  this.getCount = function () {
+    return count;
+  };
+};
+
+const myCounter = new Counter();
+myCounter.increment();
+myCounter.increment();
+console.log(myCounter.getCount());
+
+const Calculator = function () {
+  let result = 0;
+
+  this.add = function(newValue) {
+    return result += newValue;
+  };
+};
+
+const myCalculator = new Calculator();
+myCalculator.add();
