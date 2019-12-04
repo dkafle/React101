190cb6cac7ad687390f262387af1ae3d6159d00c
Deepak Kafle <dkafle@gmail.com>
2019-12-04 05:21:40 -0500

    some string related stuff

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,28 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+// console.log('webpack starterkit');
+
+const fname = 'Ramesh';
+const lname = 'Shrestha';
+
+//const fullName = fname + ' ' + lname;
+const fullName = `${fname} ${lname}`;
+
+console.log(fullName);
+console.log(fullName.charAt(3));
+console.log(fullName.lastIndexOf('e'));
+console.log(fullName.substring(0));
+// console.log(fullName[3]);
+// console.log(fullName.length);
+
+// let i = 0;
+// while (i < fullName.length) {
+//   console.log(fullName[i]);
+//   i++;
+// }
+
+const l = 'This is a very long string';
+const url = 'https://www.google.com/?q=Nepal';
+const s = url.split('/');
+console.log(s[2]);
+
