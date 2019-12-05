5bdcc61d31c78f4d1ac43823f0699daf8bd59c9e
Deepak Kafle <dkafle@gmail.com>
2019-12-04 22:35:12 -0500

    An example for data types and truithiness

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,41 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+// Premitive data types
+// let fullName = 'Deepak K';
+let age = 0;
+let address = '';
+let hasGraduated = false;
+let hobby;
+let x = undefined;
+let fruits = ['apple'];
+let person = {
+    fname: 'Deepak',
+    lname: 'Kafle'
+};
+let f = () => { }
+
+let personHasProps = Object.keys(person).length > 0;
+console.log(
+    personHasProps
+);
+
+// console.log(
+//     typeof f
+// );
+
+
+/*
+// Truithness
+let goodGuy = fruits.length > 0 || hasGraduated;
+if (goodGuy) {
+    console.log('The peson has graduaged with some fruits');
+} else {
+    console.log('go buy some fruits');
+}
+
+if (hasFruits) {
+    console.log('congrats!');
+} else {
+    console.log('');
+}
+*/
