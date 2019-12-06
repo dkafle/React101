30f9f71dc9eecc3b2101eeb64893d7b2ebf1c4c7
Deepak Kafle <dkafle@gmail.com>
2019-12-05 19:21:32 -0500

    Day 12 - Modules

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,18 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+let y = 10;
+// console.log(z);
+
+var weekDays = function () {
+  var x = 5;
+  // z = 10;
+  return {
+    myAge: x,
+    y: 'hello',
+  };
+}();
+
+console.log(
+  weekDays.myAge,
+  weekDays.y
+);
